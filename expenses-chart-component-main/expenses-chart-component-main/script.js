07adeff23d92629f180599f930f3d0c4082b40db
Josh <joshmwood95@gmail.com>
2022-06-04 12:32:54 -0400

    dynamically generate the chart's bars based on data provided

---
+++ expenses-chart-component-main/expenses-chart-component-main/script.js
@@ -0,0 +1,30 @@
+async function fillChart() {
+
+
+    fetch("data.JSON")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            let highest = 0;
+            let highestnode;
+            data.forEach(ele => {
+                console.log(ele.day);
+                console.log(ele.amount);
+                let node = document.getElementById(`${ele.day}`).children[0]
+                node.style.height = `${ele.amount * 2.5}px`
+                node.classList.add("bar__blue");
+                console.log(node);
+
+                if (ele.amount > highest) {
+                    highestnode = node;
+                    highest = ele.amount;
+                }
+
+            })
+
+            highestnode.classList.remove("bar__blue");
+            highestnode.classList.add("bar__red");
+        })
+}
+
+fillChart();
