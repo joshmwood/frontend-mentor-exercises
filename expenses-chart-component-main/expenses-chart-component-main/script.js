eae9e219bdfcc269df489431f9dc0998e07283cb
Josh <joshmwood95@gmail.com>
2022-06-07 04:02:20 -0400

    add hover states

--- expenses-chart-component-main/expenses-chart-component-main/script.js
+++ expenses-chart-component-main/expenses-chart-component-main/script.js
@@ -6,12 +6,23 @@ async function fillChart() {
             console.log(data)
             let highest = 0;
             let highestnode;
+
             data.forEach(ele => {
                 console.log(ele.day);
                 console.log(ele.amount);
-                let node = document.getElementById(`${ele.day}`).children[0]
-                node.style.height = `${ele.amount * 2.5}px`
+                let node = document.getElementById(`${ele.day}`)
+                node.style.height = `${Math.floor(ele.amount * 2.5)}px`
                 node.classList.add("bar__red");
+
+                let spendingAmount = document.createElement("div");
+                spendingAmount.innerText = `$${ele.amount}`;
+                spendingAmount.classList.add("chart--spending-amount");
+                spendingAmount.classList.add("chart--spending-amount__hidden");
+                spendingAmount.id = `${ele.day}Amount`
+
+                node.insertAdjacentElement("beforebegin", spendingAmount);
+                node.addEventListener("mouseover", showSpendingAmount);
+                node.addEventListener("mouseleave", dontShowSpendingAmount)
                 console.log(node);
 
                 if (ele.amount > highest) {
@@ -26,3 +37,14 @@ async function fillChart() {
 }
 
 fillChart();
+
+function showSpendingAmount() {
+    let id = `${this.id}Amount`;
+    document.getElementById(id).classList.add("chart--spending-amount__show");
+}
+
+function dontShowSpendingAmount() {
+    let id = `${this.id}Amount`;
+    document.getElementById(id).classList.add("chart--spending-amount__hidden");
+    document.getElementById(id).classList.remove("chart--spending-amount__show");
+}
